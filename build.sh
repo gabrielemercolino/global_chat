ce173b1d331075570c82e29fa7c98b1566f8bf35
Gabriele Mercolino <gmercolino2003@gmail.com>
2025-06-12 22:23:37 +0200

    chore: update render related stuff as for docs

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
-set -e
+#!/usr/bin/env bash
+set -o errexit
 
 mix deps.get --only prod
-
 MIX_ENV=prod mix compile
 
+MIX_ENV=prod mix assets.build
 MIX_ENV=prod mix assets.deploy
 
-MIX_ENV=prod mix phx.digest
+MIX_ENV=prod mix phx.gen.release
+MIX_ENV=prod mix release --overwrite
